ec8379ef7b82926753486e36aaeac489c31734c6
Maycol <mycolteman18ajadatilla@gmail.com>
2026-09-21 21:22:14 -0500

    creamos la clase alumno

---
+++ Documents/AAAAA/EL_Instituto_Innova/src/el_instituto_innova/Alumno.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package el_instituto_innova;
+
+/**
+ *
+ * @author UCA40403
+ */
+public class Alumno {
+    
+}
