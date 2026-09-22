85f86a70ba7bbc30c9129e50e2a2ce92c4e5a113
Maycol <mycolteman18ajadatilla@gmail.com>
2026-09-21 21:27:30 -0500

    encapsulamos

--- Documents/AAAAA/EL_Instituto_Innova/src/el_instituto_innova/Alumno.java
+++ Documents/AAAAA/EL_Instituto_Innova/src/el_instituto_innova/Alumno.java
@@ -16,5 +16,62 @@ public class Alumno {
     private String tipoBeca;
     private double pensionBase;
     private double pensionFinal;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTipoDocumento() {
+        return tipoDocumento;
+    }
+
+    public void setTipoDocumento(String tipoDocumento) {
+        this.tipoDocumento = tipoDocumento;
+    }
+
+    public String getNumeroDocumento() {
+        return numeroDocumento;
+    }
+
+    public void setNumeroDocumento(String numeroDocumento) {
+        this.numeroDocumento = numeroDocumento;
+    }
+
+    public String getNivelSocioeconomico() {
+        return nivelSocioeconomico;
+    }
+
+    public void setNivelSocioeconomico(String nivelSocioeconomico) {
+        this.nivelSocioeconomico = nivelSocioeconomico;
+    }
+
+    public String getTipoBeca() {
+        return tipoBeca;
+    }
+
+    public void setTipoBeca(String tipoBeca) {
+        this.tipoBeca = tipoBeca;
+    }
+
+    public double getPensionBase() {
+        return pensionBase;
+    }
+
+    public void setPensionBase(double pensionBase) {
+        this.pensionBase = pensionBase;
+    }
+
+    public double getPensionFinal() {
+        return pensionFinal;
+    }
+
+    public void setPensionFinal(double pensionFinal) {
+        this.pensionFinal = pensionFinal;
+    }
+    
     
 }
